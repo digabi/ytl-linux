9a9d274d319f4dfc55a789458eaa174bee529dec
Linus Willner <linus.willner@reaktor.com>
2025-04-29 17:21:24 +0300

    Improve script formatting for legibility in dpkg errors

--- packages/ytl-linux-digabi2-wsl/before-install.sh
+++ packages/ytl-linux-digabi2-wsl/before-install.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 if ! grep -qi "microsoft" /proc/version; then
   echo "Error: This does not appear to be a WSL installation. This package only works in WSL."
-  echo "You're probably looking for this package instead: ytl-linux-digabi2" 
+  echo "       You're probably looking for this package instead: ytl-linux-digabi2"
   exit 1
 fi
--- packages/ytl-linux-digabi2/before-install.sh
+++ packages/ytl-linux-digabi2/before-install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if grep -qi "microsoft" /proc/version; then
-  echo "Error: This appears to be a WSL installation. This package does not work in WSL." 
-  echo "You're probably looking for this package instead: ytl-linux-digabi2-wsl" 
+  echo "Error: This appears to be a WSL installation. This package does not work in WSL."
+  echo "       You're probably looking for this package instead: ytl-linux-digabi2-wsl"
   exit 1
 fi
