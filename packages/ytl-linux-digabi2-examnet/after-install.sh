743e2b6c5d0d936afb7f5ad8645b4b76c0fdf647
Linus Willner <linus.willner@reaktor.com>
2025-10-30 10:33:28 +0200

    Remove old NCSI script and convert systemd service to use new CLI

--- packages/ytl-linux-digabi2-examnet/after-install.sh
+++ packages/ytl-linux-digabi2-examnet/after-install.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 systemctl daemon-reload
-systemctl enable ytl-linux-digabi2-ncsi
-systemctl start ytl-linux-digabi2-ncsi
+
+# Remove old NCSI service if it exists
+if [[ -f /etc/systemd/system/ytl-linux-digabi2-ncsi.service ]]; then
+    systemctl stop ytl-linux-digabi2-ncsi
+    systemctl disable ytl-linux-digabi2-ncsi
+    rm -f /etc/systemd/system/ytl-linux-digabi2-ncsi.service
+fi
+
+systemctl enable ytl-linux-digabi2-examnet
