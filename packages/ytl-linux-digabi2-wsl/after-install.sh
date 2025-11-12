83192310192704d15db62c5a4bd66d63f99228f6
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2025-11-12 16:29:23 +0200

    Downgrade and pin docker after installing ytl-linux-digabi2-wsl
    - Re-add pinned docker versions
    - Also remove Conflicts rules to allow installation of package even when too new docker is installed

--- packages/ytl-linux-digabi2-wsl/after-install.sh
+++ packages/ytl-linux-digabi2-wsl/after-install.sh
@@ -5,3 +5,9 @@ set -euo pipefail
 # Use SUDO_USER to get actual user because fpm after install scripts are run as root
 sudo usermod -aG docker "$SUDO_USER"
 su -c "xdg-settings set default-web-browser wslview.desktop" "$SUDO_USER"
+
+## Reinstall specific versions of containerd and docker
+sudo apt install --reinstall --allow-downgrades --yes \
+  docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble \
+  docker-ce=5:28.5.2-1~ubuntu.24.04~noble \
+  containerd.io=1.7.28-1~ubuntu.24.04~noble
