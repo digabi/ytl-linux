4f98928ebb0a2394b9112e80bc1e5ca939f51735
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2025-11-12 16:28:41 +0200

    Downgrade docker after installing ytl-linux-digabi2
    - Also remove Conflicts rules to allow installation of package even when too new docker is installed

--- packages/ytl-linux-digabi2/after-install.sh
+++ packages/ytl-linux-digabi2/after-install.sh
@@ -7,4 +7,9 @@ if [[ -f /etc/docker/daemon.json ]]; then
   rm -f /etc/docker/daemon.json
 fi
 
-systemctl restart docker
+## Reinstall specific versions of containerd and docker
+sudo apt install --reinstall --allow-downgrades --yes \
+  docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble \
+  docker-ce=5:28.5.2-1~ubuntu.24.04~noble \
+  containerd.io=1.7.28-1~ubuntu.24.04~noble
+
