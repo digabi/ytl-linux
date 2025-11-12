ef0064bcc9c459885d9f8f837aa1f7fc0332eb1b
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2025-11-12 17:10:43 +0200

    Create a one-shot systemd service that downgrades docker

--- packages/ytl-linux-digabi2/after-install.sh
+++ packages/ytl-linux-digabi2/after-install.sh
@@ -7,9 +7,6 @@ if [[ -f /etc/docker/daemon.json ]]; then
   rm -f /etc/docker/daemon.json
 fi
 
-## Reinstall specific versions of containerd and docker
-sudo apt install --reinstall --allow-downgrades --yes \
-  docker-ce-cli=5:28.5.2-1~ubuntu.24.04~noble \
-  docker-ce=5:28.5.2-1~ubuntu.24.04~noble \
-  containerd.io=1.7.28-1~ubuntu.24.04~noble
-
+echo "Starting service that downgrades docker..."
+systemctl daemon-reload
+systemctl start downgrade-docker.service || true
