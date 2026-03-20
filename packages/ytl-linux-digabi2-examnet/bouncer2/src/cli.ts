f0717d0f6dc18a01da51637a5b15b396cceb67f8
Henna Haahti <84767854+aateekoohenna@users.noreply.github.com>
2026-03-20 13:46:20 +0200

    drop the bouncer cli interface
    if your name isn't ytl-linux-digabi2-examnet --daemon, you don't
    need to touch it anyway

--- packages/ytl-linux-digabi2-examnet/bouncer2/src/cli.ts
+++ packages/ytl-linux-digabi2-examnet/bouncer2/src/cli.ts
@@ -1,24 +1,37 @@
 import { z } from '@zod/zod/v4-mini'
-import { CommaSeparatedTransform, TextFileContentSchema, TextFileStringSchema } from './cli-transform.ts'
+import { ConfigSchema, SecretsSchema } from './config.ts'
+import logger from './logger.ts'
 
-export const CLIParamsSchema = z.object({
-  friendlyName: z.optional(z.string()),
-  friendlyNameFile: z.optional(TextFileStringSchema),
-
-  dnsHostname: z.optional(z.string()),
-  dnsHostnameFile: z.optional(TextFileStringSchema),
-  dnsSearchDomain: z.string(),
-
-  ncsiHostnames: z.optional(z.pipe(z.string(), CommaSeparatedTransform)),
-  ncsiHostnamesFile: z.optional(z.pipe(TextFileStringSchema, CommaSeparatedTransform)),
-
-  serverOwnIp: z.optional(z.string()),
-  serverOwnIpFile: z.optional(TextFileStringSchema),
-
-  discoveryPort: z.optional(z.coerce.number()),
-  bouncerPort: z.optional(z.coerce.number()),
-
-  tlsCertFile: TextFileContentSchema,
-  tlsKeyFile: TextFileContentSchema
+const json = z.codec(z.string(), z.unknown(), {
+  decode: (input, ctx) => {
+    try {
+      return JSON.parse(input)
+    } catch (err: unknown) {
+      ctx.issues.push({
+        code: 'invalid_format',
+        format: 'json_string',
+        input,
+        message: (err as Error).message
+      })
+      return z.NEVER
+    }
+  },
+  encode: value => JSON.stringify(value)
 })
-export type CLIParams = z.output<typeof CLIParamsSchema>
+
+export const CLIParamsSchema = z.pipe(
+  json,
+  z.object({
+    config: ConfigSchema,
+    secrets: SecretsSchema
+  })
+)
+
+export function parseArgs(args: string[]) {
+  const result = z.tuple([CLIParamsSchema]).safeParse(args)
+  if (result.success) {
+    return result.data[0]
+  }
+  logger.error(z.prettifyError(result.error))
+  return null
+}
--- packages/ytl-linux-digabi2-examnet/bouncer2/src/main.ts
+++ packages/ytl-linux-digabi2-examnet/bouncer2/src/main.ts
@@ -1,38 +1,20 @@
-import { parseArgs } from '@std/cli'
 import { bouncerApp } from './bouncer.ts'
-import { Config, ConfigSchema, SecretsSchema } from './config.ts'
 import { discoveryApp } from './discovery.ts'
 import { loggingMiddleware } from './logging.ts'
 import logger from './logger.ts'
-import { CLIParamsSchema } from './cli.ts'
-import { transformCase } from './cli-transform.ts'
+import { parseArgs } from './cli.ts'
 
-const args = await CLIParamsSchema.parseAsync(transformCase(parseArgs(Deno.args)))
+const args = parseArgs(Deno.args)
+if (!args) Deno.exit(1)
+const { config, secrets } = args
 
-const config = ConfigSchema.parse({
-  friendlyName: args.friendlyName ?? args.friendlyNameFile!,
-  canonicalHostname: args.dnsHostname ?? args.dnsHostnameFile!,
-  ncsiHostnames: args.ncsiHostnames ?? args.ncsiHostnamesFile!,
-  searchDomain: args.dnsSearchDomain,
-  serverOwnIp: args.serverOwnIp ?? args.serverOwnIpFile!,
-  ports: {
-    discovery: args.discoveryPort ?? 26464,
-    bouncer: args.bouncerPort ?? 80
-  }
-} satisfies Config)
-
-logger.info(config, `Starting digabi2-examnet-bouncer with config:`)
-
-const secrets = await SecretsSchema.parseAsync({
-  key: args.tlsKeyFile,
-  cert: args.tlsCertFile
-})
+logger.info(config, 'Starting digabi2-examnet-bouncer with config:')
 
 const _discoveryServer = Deno.serve(
   {
     port: config.ports.discovery,
-    cert: secrets.cert,
-    key: secrets.key
+    cert: await Deno.readTextFile(secrets.cert),
+    key: await Deno.readTextFile(secrets.key)
   },
   loggingMiddleware(discoveryApp(config))
 )
