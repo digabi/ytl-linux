684e5727116a275bcfded82274905a23894aa1c7
Linus Willner <linus.willner@reaktor.com>
2026-06-18 13:38:04 +0300

    Consistently manage service states on install and remove

--- packages/ytl-linux-update-checker/after-install.sh
+++ packages/ytl-linux-update-checker/after-install.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+systemctl daemon-reload
+
 systemctl enable ytl-linux-update-check.timer
 systemctl enable ytl-linux-update-check.service
+systemctl start ytl-linux-update-check.timer
