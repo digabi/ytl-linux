25d31562d2d5df7b5a15523b630a968acaa11308
Linus Willner <linus.willner@reaktor.com>
2026-07-22 13:59:05 +0300

    Make ytl-linux-customize-24 use static after-* scripts like the rest
    ...instead of using an oddball dynamic assembly process that none of the others use and that doesn't seem necessary, since all dynamically assembled script parts were already static.
    
    This rids us of the need to support the deb-scripts directory inside of a package that some of the packages supported and some didn't, depending on where the Makefile was copied from.

--- packages/ytl-linux-purge-deb/after-install.sh
+++ packages/ytl-linux-purge-deb/after-install.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # Remove deprecated configuration file
-if [ -f /etc/ytl-linux-purge-deb.conf ]; then
+if [[ -f /etc/ytl-linux-purge-deb.conf ]]; then
     rm -f /etc/ytl-linux-purge-deb.conf
 fi
