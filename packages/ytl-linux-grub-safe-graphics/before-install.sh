29b3cd26d161431491cb62f31c3b8991a776b9d5
Linus Willner <linus.willner@reaktor.com>
2026-08-06 10:22:39 +0300

    Remove old files before install to prevent dpkg conflicts

---
+++ packages/ytl-linux-grub-safe-graphics/before-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Remove old versions distributed Trojan horse style inside ytl-linux-customize-24 to prevent dpkg conflicts
+OLD_FILES_TO_REMOVE=(
+  /usr/local/share/pixmaps/ytl-grub-safe-graphics.png
+  /usr/share/applications/ytl-grub-safe-graphics-on.desktop
+  /usr/share/applications/ytl-grub-safe-graphics-off.desktop
+  /usr/share/polkit-1/actions/ytl-grub-safe-graphics.policy
+  /usr/local/sbin/ytl-grub-safe-graphics
+)
+
+for file in "${OLD_FILES_TO_REMOVE[@]}"; do
+  if [[ -f "$file" ]]; then
+    rm -f "$file"
+  fi
+done
