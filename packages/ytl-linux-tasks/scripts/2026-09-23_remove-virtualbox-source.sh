2c248703be63cf96865825101ab5071a7f0a145c
Linus Willner <linus.willner@reaktor.com>
2026-09-23 14:01:22 +0300

    Fix source file location

--- packages/ytl-linux-tasks/scripts/2026-09-23_remove-virtualbox-source.sh
+++ packages/ytl-linux-tasks/scripts/2026-09-23_remove-virtualbox-source.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if [[ -f /etc/apt/sources.list.d/virtualbox.sources ]]; then
+if [[ -f /etc/apt/sources.list.d/virtualbox-oracle.sources ]]; then
   echo "Found installed VirtualBox source, removing it"
-  rm -f /etc/apt/sources.list.d/virtualbox.sources
+  rm -f /etc/apt/sources.list.d/virtualbox-oracle.sources
 fi
